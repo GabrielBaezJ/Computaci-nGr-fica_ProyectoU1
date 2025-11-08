0701809f1f2aa09606077f3ad87699036e2ed07f
Calvache Bonifaz Gabriel Alexander <gabrielcalvache3@gmail.com>
2025-11-08 18:09:44 -0500

    V1.1 Botones

--- ReproductorMúsica/ReproductorMúsica/frmMusicPlayer.Designer.cs
+++ ReproductorMúsica/ReproductorMúsica/frmMusicPlayer.Designer.cs
@@ -29,27 +29,89 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMusicPlayer));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnReplay = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnBackward = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnPlayPause = new System.Windows.Forms.Button();
             this.picCanvas = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
             this.SuspendLayout();
             // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(9, 587);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1085, 17);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // btnReplay
+            // 
+            this.btnReplay.Image = global::ReproductorMúsica.Properties.Resources.replay;
+            this.btnReplay.Location = new System.Drawing.Point(679, 628);
+            this.btnReplay.Name = "btnReplay";
+            this.btnReplay.Size = new System.Drawing.Size(45, 38);
+            this.btnReplay.TabIndex = 6;
+            this.btnReplay.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Image = global::ReproductorMúsica.Properties.Resources.stop;
+            this.btnStop.Location = new System.Drawing.Point(412, 628);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(43, 38);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // btnBackward
+            // 
+            this.btnBackward.Image = global::ReproductorMúsica.Properties.Resources.backward;
+            this.btnBackward.Location = new System.Drawing.Point(461, 628);
+            this.btnBackward.Name = "btnBackward";
+            this.btnBackward.Size = new System.Drawing.Size(63, 38);
+            this.btnBackward.TabIndex = 4;
+            this.btnBackward.UseVisualStyleBackColor = true;
+            // 
+            // btnForward
+            // 
+            this.btnForward.Image = global::ReproductorMúsica.Properties.Resources.forward;
+            this.btnForward.Location = new System.Drawing.Point(610, 628);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(63, 38);
+            this.btnForward.TabIndex = 3;
+            this.btnForward.UseVisualStyleBackColor = true;
+            // 
+            // btnPlayPause
+            // 
+            this.btnPlayPause.Image = global::ReproductorMúsica.Properties.Resources.pause_play;
+            this.btnPlayPause.Location = new System.Drawing.Point(530, 610);
+            this.btnPlayPause.Name = "btnPlayPause";
+            this.btnPlayPause.Size = new System.Drawing.Size(74, 75);
+            this.btnPlayPause.TabIndex = 2;
+            this.btnPlayPause.UseVisualStyleBackColor = true;
+            // 
             // picCanvas
             // 
-            this.picCanvas.Location = new System.Drawing.Point(12, 12);
-            this.picCanvas.Margin = new System.Windows.Forms.Padding(4);
+            this.picCanvas.Location = new System.Drawing.Point(9, 10);
             this.picCanvas.Name = "picCanvas";
-            this.picCanvas.Size = new System.Drawing.Size(1800, 738);
+            this.picCanvas.Size = new System.Drawing.Size(1350, 571);
             this.picCanvas.TabIndex = 0;
             this.picCanvas.TabStop = false;
             // 
             // frmMusicPlayer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1821, 857);
+            this.ClientSize = new System.Drawing.Size(1155, 687);
+            this.Controls.Add(this.btnReplay);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnBackward);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnPlayPause);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.picCanvas);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmMusicPlayer";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Windows Media Player";
@@ -63,5 +125,11 @@
         #endregion
 
         private System.Windows.Forms.PictureBox picCanvas;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnPlayPause;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnBackward;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnReplay;
     }
 }
